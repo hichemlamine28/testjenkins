a0c3fe3b5bfeb37e1168f28f96ad912ef8654380
Hichem Elamine <hichem.elamine@brakpoint-technology.fr>
2020-09-23 15:32:37 +0100

    modified v1.2

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenu à la formation jenkins! CI/CD devient plus clair ! non ??");
-	System.out.println("Tag v1.2");
-	System.out.println("Tag v1.2 added");
-	System.out.println("Tag v1.2 ++ not appear");
+	System.out.println("Tag v1.2 modified");
+	System.out.println("Tag v1.2 added modified");
+	System.out.println("Tag v1.2 ++ not appear compare old v1.2");
     }
 }
