054f7a41b9c0bd2e0b73392fa20710c46a57b8b6
Hichem Elamine <hichem.elamine@brakpoint-technology.fr>
2020-09-23 13:49:59 +0100

    add demo line team

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenu à la formation jenkins! CI/CD devient plus clair ! non ??");
-	System.out.println("Tag v1.2");
-	System.out.println("Tag v1.2 added");
-	System.out.println("Tag v1.2 ++ not appear");
+	System.out.println("Tag v1.3");
+	System.out.println("Tag v1.3 added");
+	System.out.println("Tag v1.3 ++ not appear");
+	System.out.println("Tag demonstrqtion for team linkinnov")
     }
 }
